69459e6fbcd5393aac2932f045bb4be62fd0e33e
Bruno Abreu <abreuleonel64@gmail.com>
2016-12-15 17:56:11 -0200

    Some Interface Adjustments

--- public_html/src/Controller/CampaignsController.php
+++ public_html/src/Controller/CampaignsController.php
@@ -3,16 +3,23 @@ namespace App\Controller;
 
 use App\Controller\AppController;
 use Cake\ORM\TableRegistry;
+use Cake\Datasource\ConnectionManager;
 
 class CampaignsController extends AppController
 {
 	public function index() 
 	{
+		
+		
+		$this->set('pagetitle', 'Mautic Campaign Migration');
+		
 		$campaign1Table = TableRegistry::get('Campaign1');
 		$campaigns = $campaign1Table->find('all')->toArray();
 		
 
 		$this->set('campaigns', $campaigns);
+		$this->set('mautic1', $this->getDBName('mautic1'));
+		$this->set('mautic2', $this->getDBName('mautic2'));
 		
 		if($this->request->is('post')) {
 			$campaign_id = $this->request->data['campaign_id'];
@@ -22,5 +29,11 @@ class CampaignsController extends AppController
 			echo 'OK';
 			exit;
 		}
+	}
+	
+	private function getDBName(string $connection) 
+	{
+		$dbObject = ConnectionManager::get($connection);
+		return $prefix = $dbObject->config()['database'];
 	}
 }
--- public_html/src/Template/Campaigns/index.ctp
+++ public_html/src/Template/Campaigns/index.ctp
@@ -1,15 +1,19 @@
 <section>
 
 	<div class="content">
-		<form action="/campaigns/" method="post">
-			<select name="campaign_id">
-				<?php foreach($campaigns as $k => $v): ?>
-					<option value="<?= $v->id; ?>"> <?= $v->name; ?></option>
-				<?php endforeach; ?>
-			</select>
-			
-			<button type="submit" class="btn"> Migrar </button>
-		</form>
+		<div class="row">
+			<div class="column large-5 platform checks large-centered" >
+				<form action="/campaigns/" method="post">
+					<select name="campaign_id">
+						<?php foreach($campaigns as $k => $v): ?>
+							<option value="<?= $v->id; ?>"> <?= $v->name; ?></option>
+						<?php endforeach; ?>
+					</select>
+					
+					<button type="submit" class="btn"> Migrate Campaign of <b> <?= $mautic1 ?></b> to <b><?= $mautic2 ?></b></button>
+				</form>
+			</div>
+		</div>
 	</div>
 
 </section>
